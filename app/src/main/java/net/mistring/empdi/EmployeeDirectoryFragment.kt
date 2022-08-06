017aba1b1c0ffc201500ef992d12190207eeaa58
Michael Stringham <michael.stringham@vivint.com>
2022-08-06 11:08:10 -0600

    Remove second fragment. App cleanup.

--- app/src/main/java/net/mistring/empdi/EmployeeDirectoryFragment.kt
+++ app/src/main/java/net/mistring/empdi/EmployeeDirectoryFragment.kt
@@ -1,40 +1,30 @@
 package net.mistring.empdi
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import net.mistring.empdi.databinding.FragmentFirstBinding
+import androidx.fragment.app.Fragment
+import net.mistring.empdi.databinding.FragmentEmployeeDirectoryBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class EmployeeDirectoryFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentEmployeeDirectoryBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentEmployeeDirectoryBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
